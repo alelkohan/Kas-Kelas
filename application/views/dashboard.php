18e122a852acce333848194f49da5f96b57f0071
Farrel Hafizh <farrelhafizh290@gmail.com>
2024-10-19 08:59:22 +0700

    Kaa Kelas

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -30,7 +30,7 @@
 								<div class="card info-card sales-card">
 
 									<div class="card-body">
-										<h5 class="card-title">Saldo <span></span></h5>
+										<h5 class="card-title">Saldo Kas<span></span></h5>
 
 										<div class="d-flex align-items-center">
 											<div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
